62cc36c5febb11b2211a3065ee2f6bd74e78e7b0
xdev200 <xdev200200@gmail.com>
2026-05-06 20:06:22 +0530

    chore: add vercel build script for flutter web

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Downloading Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+export PATH="$PATH:`pwd`/flutter/bin"
+
+echo "Flutter version:"
+flutter --version
+
+echo "Getting dependencies..."
+flutter pub get
+
+echo "Building web..."
+flutter build web --release
